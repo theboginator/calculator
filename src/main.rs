49b22334a41a685d79e3fa5215c195e9afeb802a
jbogner <boginator55@gmail.com>
2018-11-17 15:22:56 -0500

    add preliminary layout for functions and program structure comments and stuff hopefully can get working soon :)

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 use std::io;
 
-fn readinput() -> string {
+fn readinput() -> string { //Get string from
     print!("Input string: ");
     io::stdout().flush().ok().expect("Could not flush stdout");
     let mut string: String = String::new();
@@ -18,10 +18,25 @@ fn setup(){
     let mut variables = HashMap::new();
 }
 
-fn evaluateinput(input: String){
-
+fn prelimeval(input: String) -> string{ //Pull out any variable assignments and replace their value into input
+    if input.contains('='){
+        let v: Vec<&str> = input.split('=').collect();
+    }
 }
 
 fn getvalue(input: int) -> value{
     return variables
+}
+
+fn main(){
+    setup(); //All necessary setup functions
+    let mut input = readinput(); // Get input from the keyboard
+    while(input !=  "quit"){
+        input = prelimeval(input); //Remove variable assignments/repalce variables with their value
+        output = evaluate(input); //Evaluate the remaining expression
+        print!(output); //Print the output to the screen
+        input = readinput(); //Check for new input
+    }
+
+
 }
