508d7511d2383aeb9dd59f7e01315808a10190f5
jbogner <boginator55@gmail.com>
2018-11-19 00:17:37 -0500

    now echoes input string and initializes map v 0.1.4

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,57 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 use std::io;
 
-fn readinput() -> string { //Get string from
+fn readinput() -> String { //Get string from
     print!("Input string: ");
     io::stdout().flush().ok().expect("Could not flush stdout");
-    let mut string: String = String::new();
-    let _ = std::io::stdin().read_line(&mut string);
-    return string;
+    let mut input: String = String::new();
+    let _ = std::io::stdin().read_line(&mut input);
+    input = input.trim().to_string();
+    return input;
 }
 
 fn setup(){
-    let mut variables = HashMap::new();
+
+    let mut variable_map = HashMap::new();
+    variable_map.insert(String::from("pi"), 3.14159);
 }
 
-fn prelimeval(input: String) -> string{ //Pull out any variable assignments and replace their value into input
+fn prelimeval(input: String) { //Pull out any variable assignments or variable value requests and replace their value into input
     if input.contains('='){
         let v: Vec<&str> = input.split('=').collect();
+
     }
+
 }
 
-fn getvalue(input: int) -> value{
-    return variables
+fn evaluate(input: String) { //Take an expression and evaluate, return an answer
+    if input.contains('+'){
+        let v: Vec<&str> = input.split('+').collect();
+
+    }
+    if input.contains('*'){
+        let v: Vec<&str> = input.split('*').collect();
+
+    }
+    if input.contains('/'){
+        let v: Vec<&str> = input.split('/').collect();
+
+    }
+    if input.contains('-'){
+        let v: Vec<&str> = input.split('-').collect();
+
+    }
+
 }
 
 fn main(){
     setup(); //All necessary setup functions
     let mut input = readinput(); // Get input from the keyboard
-    while(input !=  "quit"){
-        input = prelimeval(input); //Remove variable assignments/repalce variables with their value
-        output = evaluate(input); //Evaluate the remaining expression
-        print!(output); //Print the output to the screen
+    while input != "quit" {
+        //input = prelimeval(input); //Remove variable assignments/replace variables with their value
+        //output = evaluate(input); //Evaluate the remaining expression
+        //Print the output to the screen
+        print!("{}\n", input);
         input = readinput(); //Check for new input
     }
 
