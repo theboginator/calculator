e3393ea112dfddbf31034aacce42d88e8cf74292
jbogner <boginator55@gmail.com>
2018-11-19 20:39:31 -0500

    Added some new functions and stragety

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 use std::io;
 
-fn readinput() -> String { //Get string from
+fn readinput() -> String { //Get string from keyboard
     print!("Input string: ");
     io::stdout().flush().ok().expect("Could not flush stdout");
     let mut input: String = String::new();
@@ -15,34 +15,32 @@ fn readinput() -> String { //Get string from
     return input;
 }
 
-fn setup(){
-
-    let mut variable_map = HashMap::new();
-    variable_map.insert(String::from("pi"), 3.14159);
-}
-
 fn prelimeval(input: String) { //Pull out any variable assignments or variable value requests and replace their value into input
     if input.contains('='){
         let v: Vec<&str> = input.split('=').collect();
+        let key: &str = v[1];
+        let val: &str = v[2];
 
     }
 
 }
 
 fn evaluate(input: String) { //Take an expression and evaluate, return an answer
-    if input.contains('+'){
+    let mut expression = input.split(" "); //Split the input then we will extract the numbers and operator
+
+    if expression.contains('+'){
         let v: Vec<&str> = input.split('+').collect();
 
     }
-    if input.contains('*'){
+    if expression.contains('*'){
         let v: Vec<&str> = input.split('*').collect();
 
     }
-    if input.contains('/'){
+    if expression.contains('/'){
         let v: Vec<&str> = input.split('/').collect();
 
     }
-    if input.contains('-'){
+    if expression.contains('-'){
         let v: Vec<&str> = input.split('-').collect();
 
     }
@@ -50,15 +48,19 @@ fn evaluate(input: String) { //Take an expression and evaluate, return an answer
 }
 
 fn main(){
-    setup(); //All necessary setup functions
+    let mut variable_map = HashMap::new(); //declare the variable hashmap
+    variable_map.insert(String::from("pi"), 3.14159); //make our first constant, pi
     let mut input = readinput(); // Get input from the keyboard
     while input != "quit" {
+        prelimeval(input);
         //input = prelimeval(input); //Remove variable assignments/replace variables with their value
         //output = evaluate(input); //Evaluate the remaining expression
         //Print the output to the screen
         print!("{}\n", input);
         input = readinput(); //Check for new input
     }
-
+    for (key, value) in variable_map{
+        print!("{} / {}", key, value);
+    }
 
 }
